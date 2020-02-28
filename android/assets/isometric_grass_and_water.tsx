77b530c91c2b8c84074d253bc0e3e812b9e3ad27
Roque1211 <roque1211@gmail.com>
2020-02-29 00:51:26 +0100

    Battleship Casa 2

---
+++ android/assets/isometric_grass_and_water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="isometric_grass_and_water" tilewidth="32" tileheight="32" tilecount="96" columns="8">
+ <image source="C:/Program Files/Tiled/examples/isometric_grass_and_water.png" width="256" height="384"/>
+</tileset>
