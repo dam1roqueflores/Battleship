2a0127d111acf9cebe977a077682b50b7d662fb2
Roque1211 <roque1211@gmail.com>
2020-04-09 10:45:34 +0200

    09/04/2020

--- core/src/com/mygdx/game/Tablero.java
+++ core/src/com/mygdx/game/Tablero.java
@@ -9,152 +9,162 @@ public class Tablero {
     ////////////////////////////////////////////////////////////////////////////
     ////////////////Estado/////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////////
+    protected final static int tamtablero=10;
+    protected final static float margen=0.1f;
+
 
     protected int posX; //pos x del tablero
     protected int posY; //pos y del tablero
+    protected int tam_cel_x;
+    protected int tam_cel_y;
+    protected  int alto;
+    protected int ancho;
     protected Texture imagen;
-    protected int contpintar;
+    protected Texture imagenpulsada;
+    //protected int contpintar;
+
+
+
     protected Flota miflota; //flota aliada
-    protected Celda micelda; //celda con agua
+    //protected Celda micelda; //celda con agua
+
+    protected boolean[][] mapa;
+    //ArrayList<ArrayList<Boolean>> mapa;
 
 
-    ArrayList<ArrayList<Boolean>> mapabooleano;
-
-
-    private static final short FILAS=10; //nº de filas
-    private static final short COLUMNAS=10; //nº de columnas
-    private static final short LADO=10;
+    //private static final short FILAS=10; //nº de filas
+    //private static final short COLUMNAS=10; //nº de columnas
+    //private static final short LADO=10;
     private static final short TASACAMBIO=20;
     private static final String FILEAGUA1="agua1.png"; //imagen para celda
-    private static final String FILEAGUA2="agua2.png";
+    //private static final String FILEAGUA2="agua2.png";
     private static final String FILEAGUAPULSADA1="aguapulsada1.png";
-    private static final String FILEAGUAPULSADA2="aguapulsada2.png";
+    //private static final String FILEAGUAPULSADA2="aguapulsada2.png";
 
 
 
     //comportamiento
     //contructor
-    public Tablero(int posXtablero,int posYtablero){//,boolean Flotatype){
-        posX=posXtablero;
-        posY=posYtablero;
+    public Tablero(int posXtablero,int posYtablero,int altoDisponible,int anchoDisponible){//,boolean Flotatype){
 
+        //contpintar=0;
 
-        contpintar=0;
+        imagen=new Texture(FILEAGUA1);
+        imagenpulsada= new Texture(FILEAGUAPULSADA1);
 
-        //creamos array list con toda array list
+        posX=(int)(posXtablero*margen);
+        posY=(int)(posYtablero*margen);
 
-        for (int k=1;k<FILAS;k++){
-            mapabooleano.add(new ArrayList());
-        }
+        alto=altoDisponible;
+        ancho=anchoDisponible;
+
+        tam_cel_x=(int)(anchoDisponible * (1.0f-margen*2.0f)/tamtablero);
+        tam_cel_x=(int)(altoDisponible * (1.0f-margen*2.0f)/tamtablero);
+
+        //for (int k=1;k<tamtablero;k++){
+        //  mapa.add(new ArrayList());
+        //}
 
         //llenamos la array list multidimesional
 
-        for (int cont1=0;cont1<FILAS;cont1++){
+        //for (int cont1=0;cont1<tamtablero;cont1++){
 
-            for (int cont2=0;cont2<COLUMNAS;cont2++){
-                mapabooleano.get(cont1).add(true);
-            }
-        }
+        //  for (int cont2=0;cont2<tamtablero;cont2++){
+        //    mapa.get(cont1).add(true);
+        //}
+        //}
+        mapa= new  boolean[tamtablero][tamtablero];
 
         //instanciamos la flota
-        miflota= new Flota(FILAS,COLUMNAS,posXtablero,posYtablero,LADO);
+        //miflota= new Flota(tamtablero,tamtablero,posXtablero,posYtablero,tam_cel_x);
 
         //inicializamos imagen
 
-        imagen= new Texture(FILEAGUA1);
 
         //iniciamos la celda
 
-        micelda= new Celda(posX,posY,FILEAGUA1);
+        //micelda= new Celda(posX,posY,FILEAGUA1);
 
-
+        reset();
 
     }
 
 
     //resto de comportamientos
-    public boolean comprobarDisparo(int posxdisp,int posydisp){
-        boolean miboolean;
-        int filas=traducirfila(posydisp);
-        int columnas=traducircolumna(posxdisp);
-
-        if (mapabooleano.get(filas).get(columnas)==true){// || flota.ispulsado()==true){ //or flota ispulsado==true
-            miboolean=true;
-        } else {
-            miboolean=false;
-        }
-        return miboolean;
-    }
+    //public boolean comprobarDisparo(int posxdisp,int posydisp){
+    //  boolean miboolean;
+    // int filas=traducirfila(posydisp);
+    //int columnas=traducircolumna(posxdisp);
 
 
-    public void pintarse(SpriteBatch miSB,int posxcelda,int posycelda){
+
+    //if (mapa.get(filas).get(columnas)==true){// || flota.ispulsado()==true){ //or flota ispulsado==true
+    //      miboolean=true;
+    //} else {
+    //  miboolean=false;
+    // }
+    //return miboolean;
+    //}
 
 
-        String texturapintar=FILEAGUA1;
+    public void pintarse(SpriteBatch miSB){
 
+        //pìntamos el agua
+        pintar_escenario(miSB);
 
-        //necesitro recorer la array list para pintarlo
-        //y entonces pintar cada "casilla" segun sea true o false
-        //bucle para pintar array multidimensional
-
-
-        for (int cont1=0;cont1<FILAS;cont1++){
-
-            for (int cont2=0;cont2<COLUMNAS;cont2++){
-                if (mapabooleano.get(cont1).get(cont2)==true){
-                    //SI ES TRUE
-
-                    if (contpintar<TASACAMBIO){
-                        texturapintar=FILEAGUA1;
-                        contpintar++;
-                    } else if (contpintar>(TASACAMBIO*2)){
-                        texturapintar=FILEAGUA2;
-                        contpintar++;
-                    } else {
-                        contpintar=0;
-                    }
-
-                } else {
-                    //SI ES FALSE
-                    if (contpintar<TASACAMBIO){
-                        texturapintar=FILEAGUAPULSADA1;
-                        contpintar++;
-                    } else if (contpintar>(TASACAMBIO*2)){
-                        texturapintar=FILEAGUAPULSADA2;
-                        contpintar++;
-                    } else {
-                        contpintar=0;
-                    }
-                }
-                micelda.pintarse(posX+(cont1*LADO),posY+(cont2*LADO),texturapintar)); //tectura agua pulsada
-            }
-
-            //  micelda.pintarse(posX+(cont1*LADO),posy+(cont2*LADO)textura;
-            //pinto celdas en base la array de true o false
-            //celda.pintarse(posX+(cont1*LADO),posY+(cont2*LADO),imagen);
-        }
-
-
-        //luego se pint        a la flota
+        //pintamos la flota
         miflota.pintarse(miSB);
 
+
+    }
+
+    private void pintar_escenario(SpriteBatch miSB){
+        for (int cont1=0;cont1<tamtablero;cont1++){
+            for (int cont2=0;cont2<tamtablero;cont2++){
+                miSB.begin();
+                if (mapa[cont1][cont2]=true){
+                    miSB.draw(imagen,posX+tam_cel_x*cont1,posY+tam_cel_y*cont2);
+                } else {
+                    miSB.draw(imagenpulsada,posX+tam_cel_x*cont1,posY+tam_cel_y*cont2);
+                }
+            }
+        }
+    }
+
+    public void reset(){
+        for (int o=0;o<tamtablero;o++){
+            for (int f=0;f<tamtablero;f++){
+                mapa[o][f]= true;
+            }
+        }
+        miflota= new Flota(tamtablero,tamtablero,posX,posY,tam_cel_x);
     }
 
 
-    private int traducirfila(int pulposy){
-        int fila;
-        fila=posY+(pulposy/LADO); //yini(tablero)+(pulposy/lado)
-        return fila;
-    }
 
-    private int traducircolumna(int pulposx){
-        int columna;
-        columna=posX+(pulposx/LADO); //yini(tablero)+(pulposy/lado)
-        return columna;
+
+    public boolean comprobarDisparo(int pulposx,int pulposy){
+        int casillax,casillay;
+        boolean resultado=true;
+
+        casillax= ((pulposx - posX)/tam_cel_x);
+        casillay= ((pulposy - posY)/tam_cel_y);
+
+        if (mapa[casillax][casillay]){
+            resultado=false;
+        } else {
+            mapa[casillax][casillay]=false;
+            //resultado=miflota.disparo[casillax][casillay];
+            resultado=false;
+
+        }
+
+        return resultado;
     }
 
     public void dispose(){
         imagen.dispose();
+        imagenpulsada.dispose();
         miflota.dispose();
     }
 
